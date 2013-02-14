a1aed2b0115e7ab4bb13a760d7f6a739172ba975
Johan Martinsson <martinsson.johan@gmail.com>
2013-02-14 22:12:31 +0100

    changed testfile to be asserting the same contents as the reference file

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/SmallGoldenMaster.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/SmallGoldenMaster.java
@@ -1,21 +1,31 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.io.File;
 import java.io.PrintStream;
 import java.util.Random;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import com.adaptionsoft.games.trivia.runner.GameRunner;
 
 public class SmallGoldenMaster {
+    
+    @Rule public TemporaryFolder tmp = new TemporaryFolder();
+    private static final String REFERENCE_FILE_BASE = "src/test/resources/" + "long/" + "Reference.";
 
     @Test
     public void test() throws Exception {
         Random randomizer = new Random(287389);
         
         for (int i = 0; i < 200; i++) {
-            System.setOut(new PrintStream("src/test/resources/" + "short/" + "Reference."+i));
+            File file = tmp.newFile();
+            System.setOut(new PrintStream(file));
             GameRunner.playGame(randomizer);
+            assertThat(file).hasContentEqualTo(new File(REFERENCE_FILE_BASE + i));
         }
         
     }
