5ebb85872a34dab4d8fad28877ba5a6908acbfbd
Johan Martinsson <martinsson.johan@gmail.com>
2014-05-19 00:54:22 +0200

    fix indentation

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,35 +1,32 @@
-
 package com.adaptionsoft.games.trivia.runner;
+
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
 
-
 public class GameRunner {
 
-	private static boolean notAWinner;
+    private static boolean notAWinner;
 
-	public static void main(String[] args) {
-		Random random = new Random();
-     
-		Game aGame = new Game();
-        aGame.add("Chet");
-        aGame.add("Pat");
-        aGame.add("Sue");
-        
-        do {
-        	
-        	aGame.roll(random.nextInt(5) + 1);
-        	
-        	if (random.nextInt(9) == 7) {
-        		notAWinner = aGame.wrongAnswer();
-        	} else {
-        		notAWinner = aGame.wasCorrectlyAnswered();
-        	}
-        	
-        	
-        	
-        } while (notAWinner);
-		
-	}
+    public static void main(String[] args) {
+	Random random = new Random();
+
+	Game aGame = new Game();
+	aGame.add("Chet");
+	aGame.add("Pat");
+	aGame.add("Sue");
+
+	do {
+
+	    aGame.roll(random.nextInt(5) + 1);
+
+	    if (random.nextInt(9) == 7) {
+		notAWinner = aGame.wrongAnswer();
+	    } else {
+		notAWinner = aGame.wasCorrectlyAnswered();
+	    }
+
+	} while (notAWinner);
+
+    }
 }
