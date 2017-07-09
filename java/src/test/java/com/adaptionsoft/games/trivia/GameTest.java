8125a7457dc016504ff3b694c2c68f2722052904
Johan MARTINSSON <martinsson.johan@gmail.com>
2017-07-10 00:02:28 +0200

    just reduced some duplication and simplified conditional logic

--- java/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.*;
 
 import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.approvaltests.Approvals;
+import org.approvaltests.reporters.UseReporter;
+import org.approvaltests.reporters.macosx.MacDiffReporter;
 import org.junit.Test;
 
 import java.io.*;
 import java.util.Random;
 import java.util.stream.IntStream;
 
+@UseReporter(MacDiffReporter.class)
 public class GameTest {
 
 	@Test
